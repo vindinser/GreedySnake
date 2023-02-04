286a2fdd6e0a139602f1faabf90a192fb1f98b82
zhangshuang <zhangshuang@hzdf.com>
2023-02-04 15:40:18 +0800

    初步完成Snake类

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,9 @@ import './style/index.less'
 // 引入类
 import Food from './moduls/Food'
 import ScorePanel from './moduls/ScorePanel'
+import Snake from './moduls/Snake'
 
 console.log('%cGreedySnake', 'color: red; font-size: 20px; font-weight: 600;');
 
-const scorePanel = new ScorePanel();
-for(let i = 0; i < 200; i++) {
-  scorePanel.addScore()
-}
-
-const food = new Food()
-console.log(food.X, food.Y)
-food.change()
-console.log(food.X, food.Y)
+const snake = new Snake
+console.log(snake)
---
+++ src/moduls/Snake.ts
@@ -0,0 +1,41 @@
+// 蛇
+class Snake {
+  // 蛇元素(蛇头)
+  snakeHeadEle: HTMLElement;
+  // 蛇元素（身体，包括蛇头）
+  snakeBodyEle: HTMLCollection;
+  // 蛇的容器
+  snakeAllEle: HTMLElement;
+
+  constructor() {
+    this.snakeAllEle = document.querySelector('.snake')!
+    this.snakeHeadEle = document.querySelector('.snake > div') as HTMLElement;
+    this.snakeBodyEle = this.snakeAllEle.getElementsByTagName('div');
+  }
+
+  // 获取蛇头x轴的坐标
+  get X() {
+    return this.snakeHeadEle.offsetLeft
+  }
+  // 获取蛇头y轴的坐标
+  get Y() {
+    return this.snakeHeadEle.offsetTop
+  }
+
+  // 设置蛇头x轴坐标
+  set X(val) {
+    this.snakeHeadEle.style.left = `${val}px`
+  }
+
+  // 设置蛇头x轴坐标
+  set Y(val) {
+    this.snakeHeadEle.style.top = `${val}px`
+  }
+
+  // 蛇增加长度
+  addSnakeBody() {
+    this.snakeAllEle.insertAdjacentHTML('beforeend', '<div></div>')
+  }
+}
+
+export default Snake;
